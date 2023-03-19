43cc01d87e58a63ce78845e7c1bfdcabb86bb878
Tabetha Moe <icreatetheinternet@gmail.com>
2023-03-19 15:27:22 -0400

    set up basic block and added linting for js and scss

---
+++ app/public/wp-content/plugins/hero-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e1f71a0f0e09b49292db');
---
+++ app/public/wp-content/themes/tabbyknits/searchform.php
@@ -0,0 +1,10 @@
+<?php
+// This will be used as the search template wherever the search shows up, in a widget, or in the template by using the following hook:
+// get_search_form( true );
+//
+?>
+<form method="get" id="searchform" class="search-box" action="<?php echo home_url(); ?>/" class="form-inline">
+    <label class="sr-only" for="s"><?php _e('Search:', 'tabbyknits'); ?></label>
+    <input type="text" value="<?php the_search_query(); ?>" placeholder=" " name="s" id="s" />
+    <button type="reset"></button>
+</form>
